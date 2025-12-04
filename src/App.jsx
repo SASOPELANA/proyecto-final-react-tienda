d652b7ea9943753c65a10b6b3cfde2a253e622d4
Sergio <sopekof@gmail.com>
2025-12-04 14:58:28 -0300

    proyecto final

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Productos from "./components/Products";
+
+function App() {
+  return (
+    <div>
+      <Productos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/types/products.types.js
@@ -0,0 +1,9 @@
+// Solo para autocompletado, nada ayuda
+export const ProductAyuda = {
+  id: "",
+  name: "",
+  description: "",
+  price: 0,
+  image: "",
+  categories: [],
+};
